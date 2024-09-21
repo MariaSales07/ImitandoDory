3ef94c432d2e76d3e78b351557edafee75a5514e
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-09-20 22:39:59 -0300

    programa finalizado

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("---Imitando Dory---\n");
+
+Console.Write("Digite uma frase:");
+string frase = Console.ReadLine()!;
+
+string fraseEmBaleies = frase
+        .Replace ("a", "aaa")
+        .Replace ("A", "AAA")
+        .Replace ("E", "EEE")
+        .Replace ("e", "eee")
+        .Replace ("o", "ooo")
+        .Replace ("O", "OOO")
+        .Replace ("I", "III")
+        .Replace ("i", "iii")
+        ;
+
+Console.WriteLine($"\nEm baleies:\n\n{fraseEmBaleies}");
